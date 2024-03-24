f79572d03c16e48bb1394a5816204f4b28ab6226
haitammabrouk <89913263+haitammabrouk@users.noreply.github.com>
2024-03-24 17:14:36 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,17 @@
 import java.util.List;
-import java.io.BufferedReader; 
-import java.io.IOException; 
-import java.io.InputStreamReader;
 
 class Main {
-  public static void main(String[] args) throws IOException{
-    System.out.println("Please input a mathematical expression : ");
-    while(true){
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-        String name = reader.readLine(); 
-        Scanner sc = new Scanner(name);
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Usage: java Main <mathematical_expression>");
+            System.exit(1);
+        }
+
+        String expression = args[0];
+        Scanner sc = new Scanner(expression);
         List<Token> scanExp = sc.scan();
         Parser parser = new Parser(scanExp);
         List<Token> parsed = parser.parse();
         System.out.println(sc.eval(parsed));
     }
-  }
 }
